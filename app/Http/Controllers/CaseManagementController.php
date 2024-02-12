0a23a8338d688b038ec2d751ec0bcc266c9c6b5b
uche lebechi <lebechiuchey@gmail.com>
2024-02-12 09:48:10 +0100

    fix

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -1712,7 +1712,6 @@ class CaseManagementController extends Controller
                     ]);
                 }
             }
-
             return response()->json([
                 $result,
                 'message' => 'Query Execution Was Successful.'
